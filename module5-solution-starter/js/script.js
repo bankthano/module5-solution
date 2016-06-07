869d6a2d691aeaf32da58529f9833b1326bdbc3b
bankthano <bank_60@hotmail.com>
2016-06-07 13:27:35 +0700

    13-27

--- module5-solution-starter/js/script.js
+++ module5-solution-starter/js/script.js
@@ -94,15 +94,16 @@ $ajaxUtils.sendGetRequest(
 function buildAndShowHomeHTML (categories) {
   
   // Load home snippet page
+
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtmlUrl) {
-
+    function (homeHtml) {
+      document.querySelector("#main-content").innerHTML = homeHtml;
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log('choosCategoryShortName: ' +choosCategoryShortName);
+      console.log('choosCategoryShortName: ' + choosCategoryShortName);
       
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
